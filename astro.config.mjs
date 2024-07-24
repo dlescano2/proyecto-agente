cafc60777dc606a00d3afef77e1e33e413d8d002
dlescano2 <dlescano2@gmail.com>
2024-07-24 09:32:29 -0300

    Actu

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,4 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: 'https://github.com/dlescano2/proyecto-agente.git',
-  base: '/proyecto-agente.git/'
 });
